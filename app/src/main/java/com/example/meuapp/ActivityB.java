727fb27ae35c47ce6ebd9e02cd1760578ba32b26
luiz <luiz.m2008@aluno.ifsc.edu.br>
2026-04-15 12:10:56 -0300

    asdfasdgndfvzxs

--- app/src/main/java/com/example/meuapp/ActivityB.java
+++ app/src/main/java/com/example/meuapp/ActivityB.java
@@ -1,6 +1,9 @@
 package com.example.meuapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity2 extends AppCompatActivity {
+import org.w3c.dom.Text;
+
+public class ActivityB extends AppCompatActivity {
+
+    Button botaoFinish;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +28,17 @@ public class MainActivity2 extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        tv=findViewById(R.id.textView2);
+        Intent i = getIntent();
+        Bundle b=i.getExtras();
+        String msg = b.getString("msg");
+        tv.setText(msg);
+
+        botaoFinish=findViewById(R.id.botao);
+        botaoFinish.setOnClickListener(v -> {
+            finish();
+        });
+
     }
 }
--- app/src/main/java/com/example/meuapp/MainActivity.java
+++ app/src/main/java/com/example/meuapp/MainActivity.java
@@ -1,22 +1,34 @@
 package com.example.meuapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button b;
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_b);
+        editText=findViewById(R.id.textView);
+        b=findViewById(R.id.button);
+        b.setOnClickListener( v -> {
+            Intent i = new Intent(getApplicationContext(),ActivityB.class);
+            String msg = editText.getText().toString();
+
+            i.putExtra("msg",msg);
+            startActivity(i);
+        });
+
+
 
     }
 }
