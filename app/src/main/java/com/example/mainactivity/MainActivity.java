0fd2674f4b63026f3e36e2c6750e8c710669dc28
Luiz <luiz.m2008@aluno.ifsc.edu.br>
2026-05-20 11:59:13 -0300

    mensagem

--- app/src/main/java/com/example/mainactivity/MainActivity.java
+++ app/src/main/java/com/example/mainactivity/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.mainactivity;
 
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.text.Layout;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 
@@ -14,18 +19,33 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
 
-    int contador = 0;
+    SQLiteDatabase sqLiteDatabase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sqLiteDatabase = openOrCreateDatabase("notas", MODE_PRIVATE, null);
 
-        TextView tv = findViewById(R.id.tv);
-        Button b = findViewById(R.id.button);
-        b.setOnClickListener(v -> {
-            contador++;
-            tv.setText(Integer.toString(contador));
+        sqLiteDatabase.execSQL("CREATE TABLE IF NOT EXISTS notas (id INTEGER PRIMARY KEY AUTOINCREMENT, titulo TEXT, nota TEXT )");
+//        String titulo="joão' ; TRUCATE TABLE;'";
+//        sqLiteDatabase.execSQL("INSERT INTO notas VALUES (1, ' "+titulo+" ', '')");
 
-        });
+        ContentValues contentValues = new ContentValues();
+        contentValues.put("titulo", "joao");
+        contentValues.put("nota", "777777777777");
+
+        sqLiteDatabase.insert("notas", null, contentValues);
+
+
+        Cursor c= sqLiteDatabase.rawQuery("SELECT id,id*10,titulo,nota FROM notas", null);
+        c.moveToFirst();
+
+        while (!c.isAfterLast()) {
+            @SuppressLint("Range") int id = c.getInt(c.getColumnIndex("id"));
+            @SuppressLint("Range") String titulo = c.getString(c.getColumnIndex("titulo"));
+            @SuppressLint("Range") String nota = c.getString(c.getColumnIndex("nota"));
+            Log.d("SELECT", Integer.toString(id) + "," + titulo + "," + nota);
+            c.moveToNext();
+        }
     }
 }
